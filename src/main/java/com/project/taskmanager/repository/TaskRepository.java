debcb301ee93b3568b2edb1ab3e9a9092509048d
211451 <srinuthatikonda.rao@htmedialabs.com>
2026-04-12 13:04:36 +0530

    Basic Crud Operations For Task Manager

--- src/main/java/com/project/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/project/taskmanager/repository/TaskRepository.java
@@ -1,8 +1,13 @@
 package com.project.taskmanager.repository;
 
 import com.project.taskmanager.model.Task;
+import org.jspecify.annotations.Nullable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 
-public interface TaskRepository extends MongoRepository<Task, Integer> {
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+    @Nullable List<Task> getByCompleted(Boolean completed);
 }
--- src/main/java/com/project/taskmanager/service/TaskService.java
+++ src/main/java/com/project/taskmanager/service/TaskService.java
@@ -5,8 +5,12 @@ import com.project.taskmanager.repository.TaskRepository;
 import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import java.awt.*;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -20,5 +24,37 @@ public class TaskService {
         task.setTitle(task.getTitle());
         task.setDescription(task.getDescription());
         return taskRepository.save(task);
+    }
+
+    public @Nullable Task updateTask(Task task) {
+        Optional<Task> updateData = taskRepository.findById(task.getId());
+        if(updateData.isPresent()){
+            Task task1 = updateData.get();
+            task1.setTitle(task.getTitle());
+            task1.setDescription(StringUtils.hasLength(task.getDescription()) ? task.getDescription() : task1.getDescription());
+            task1.setCompleted(task.getCompleted());
+            return taskRepository.save(task1);
+        }
+        return null;
+    }
+
+    public boolean deleteTask(String id) {
+         taskRepository.deleteById(id);
+         return true;
+    }
+
+    public @Nullable List<Task> getTasks(Boolean completed) {
+        return taskRepository.getByCompleted(completed);
+    }
+
+    public @Nullable String updateFields(String id, Boolean completed) {
+        Optional<Task> existingTaskData = taskRepository.findById(id);
+        if(existingTaskData.isPresent()) {
+            Task task = existingTaskData.get();
+            task.setCompleted(completed);
+            taskRepository.save(task);
+        }
+
+        return "Status Got Updated SuccessFully";
     }
 }
