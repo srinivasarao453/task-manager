19ae76333f1413cb84a2656e41cb1682140c221c
Srinivasa Rao Thatikonda <srinuthatikonda453@gmail.com>
2026-04-14 11:21:16 +0530

    task manager api changes

--- src/main/java/com/project/taskmanager/controller/TaskController.java
+++ src/main/java/com/project/taskmanager/controller/TaskController.java
@@ -2,12 +2,12 @@ package com.project.taskmanager.controller;
 
 import com.project.taskmanager.model.Task;
 import com.project.taskmanager.service.TaskService;
-import org.apache.tomcat.util.http.parser.TE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/task")
@@ -19,24 +19,5 @@ public class TaskController {
     @PostMapping
     public ResponseEntity<Task> createTask(@RequestBody Task task){
         return ResponseEntity.ok(taskService.createTask(task));
-    }
-
-    @PutMapping
-    public ResponseEntity<?> updateTask(@RequestBody Task task){
-        return ResponseEntity.ok(taskService.updateTask(task));
-    }
-
-    @DeleteMapping("/{id}")
-    public  ResponseEntity<Boolean> deleteTask(@PathVariable String id ){
-        return ResponseEntity.ok(taskService.deleteTask(id));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Task>> getTasks(@RequestParam(required = true) Boolean completed){
-        return ResponseEntity.ok(taskService.getTasks(completed));
-    }
-    @PatchMapping("/{id}/complete")
-    public ResponseEntity<?> updateField(@PathVariable String id,@RequestParam Boolean completed){
-        return ResponseEntity.ok(taskService.updateFields(id,completed));
     }
 }
