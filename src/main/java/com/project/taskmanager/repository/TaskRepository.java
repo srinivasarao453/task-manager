7251df9bdd6b33ffbac16180243f688b66e9bacf
Srinivasa Rao Thatikonda <srinuthatikonda453@gmail.com>
2026-04-18 20:56:27 +0530

    update changes for task manager

--- src/main/java/com/project/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/project/taskmanager/repository/TaskRepository.java
@@ -1,8 +1,14 @@
 package com.project.taskmanager.repository;
 
 import com.project.taskmanager.model.Task;
+import jakarta.annotation.Nullable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
-public interface TaskRepository extends MongoRepository<Task, Integer> {
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+    @Nullable List<Task> getByCompleted(Boolean completed);
 }
